e9684ffe12f4a2601706b389883c763b3c2b0e83
priya.ashwin <priya.ashwin@olmsystems.com>
2024-06-11 16:47:59 +0100

    images added

--- src/gallery.js
+++ src/gallery.js
@@ -4,6 +4,12 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import Image1 from "./assets/image1.jpg";
 import Image2 from "./assets/image2.jpg";
+import Image3 from "./assets/image3.jpg";
+import Image4 from "./assets/image4.jpg";
+import Image5 from "./assets/image5.jpg";
+import Image6 from "./assets/image6.jpg";
+import Image8 from "./assets/image8.jpg";
+import Image9 from "./assets/image9.jpg";
 
 export default function Gallery() {
   return (
@@ -13,6 +19,24 @@ export default function Gallery() {
       </div>
       <div>
         <img src={Image2} alt="img2" />
+      </div>
+      <div>
+        <img src={Image3} alt="img3" />
+      </div>
+      <div>
+        <img src={Image4} alt="img4" />
+      </div>
+      <div>
+        <img src={Image5} alt="img5" />
+      </div>
+      <div>
+        <img src={Image6} alt="img6" />
+      </div>
+      <div>
+        <img src={Image8} alt="img8" />
+      </div>
+      <div>
+        <img src={Image9} alt="img9" />
       </div>
     </Carousel>
   );
